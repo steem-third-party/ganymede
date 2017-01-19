33a75d841a71c482d61e4b1bd0e74b46c3b39029
Anthony Martin <github@martin-studio.com>
2017-01-19 13:04:44 -0800

    validation message

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -7,23 +7,27 @@ class FollowsController < ApplicationController
     count = -1
 
     if !!@op
-      _op, _type, _mapping = case @op
-      when 'followers' then
-        [:get_followers, 'blog', :follower]
-      when 'followings' then
-        [:get_following, 'blog', :following]
-      when 'ignores' then
-        [:get_following, 'ignore', :following]
-      when 'ignoring' then
-        [:get_followers, 'ignore', :follower]
-      end
-      
-      until count == @follows.size
-        count = @follows.size
-        response = follow_api_execute(_op, @account, @follows.last, _type, 100)
-        @follows += response.result.map(&_mapping)
-        @follows = @follows.uniq
-        sleep 1
+      if !!@account
+        _op, _type, _mapping = case @op
+        when 'followers' then
+          [:get_followers, 'blog', :follower]
+        when 'followings' then
+          [:get_following, 'blog', :following]
+        when 'ignores' then
+          [:get_following, 'ignore', :following]
+        when 'ignoring' then
+          [:get_followers, 'ignore', :follower]
+        end
+        
+        until count == @follows.size
+          count = @follows.size
+          response = follow_api_execute(_op, @account, @follows.last, _type, 100)
+          @follows += response.result.map(&_mapping)
+          @follows = @follows.uniq
+          sleep 1
+        end
+      else
+        redirect_to follows_url, notice: "Please provide a user to check #{@op}."
       end
     end
   end
