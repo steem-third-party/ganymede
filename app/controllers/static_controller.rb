85278393f91bb28b62f4b2c15dce892a83755c12
Anthony Martin <github@martin-studio.com>
2017-01-22 13:54:45 -0800

    default favicon

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
 class StaticController < ApplicationController
   def index
   end
+  
+  def favicon
+    favicon = File.open "#{Rails.root}/app/assets/images/ganymede.svg"
+    
+    send_file favicon, type: 'image/svg', disposition: 'inline'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static#index'
+  get :favicon, to: 'static#favicon'
   
   resources :static, only: :index
   resources :discussions, only: :index
