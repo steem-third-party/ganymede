5ee16676045f74a7f1046cffccb4d1fc8d17b39c
Anthony Martin <github@martin-studio.com>
2017-01-23 10:27:39 -0800

    proper detection of negative rep

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -262,8 +262,10 @@ private
 
   def to_rep(raw)
     raw = raw.to_i
+    neg = raw < 0
     level = Math.log10(raw.abs)
     level = [level - 9, 0].max
+    level = (neg ? -1 : 1) * level
     level = (level * 9) + 25
     level.to_i
   end
