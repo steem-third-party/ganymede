ba4a60a6a509986298418c19f3116865d42506a8
Anthony Martin <github@martin-studio.com>
2017-01-24 12:47:32 -0800

    vote details

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,6 +1,8 @@
 module AccountsHelper
   def account_active_class(current_tab, classes = [])
     classes << case current_tab
+    when :upvoted
+      'active' if @upvoted == 'true'
     when :downvoted
       'active' if @downvoted == 'true'
     end
