4afc995b6f8d384ea8838c2977e11b6730ac529c
Anthony Martin <github@martin-studio.com>
2017-01-29 01:44:28 -0800

    typo

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -3,28 +3,26 @@ class TransfersController < ApplicationController
     @account = params[:account].presence || nil
     @transfers = []
     
-    if @account.nil?
-      redirect_to transfers_url, notice: "Please provide a user to check transactions." and return
-    end
-    
-    response = api_execute(:get_account_history, @account, 2000, 2000)
-    response.result.each do |history|
-      next unless history.last.op.first == 'transfer'
-      op = history.last.op.last
-      
-      @transfers << {
-        trx_id: history.last.trx_id,
-        timestamp: Time.parse(history.last.timestamp + 'Z'),
-        from: op.from,
-        to: op.to,
-        amount: op.amount,
-        memo: op.memo
-      }
-      
-      respond_to do |format|
-        format.html { render 'index', layout: action_name != 'card' }
-        format.atom { render layout: false }
-        format.rss { render layout: false }
+    if !!@account
+      response = api_execute(:get_account_history, @account, 2000, 2000)
+      response.result.each do |history|
+        next unless history.last.op.first == 'transfer'
+        op = history.last.op.last
+        
+        @transfers << {
+          trx_id: history.last.trx_id,
+          timestamp: Time.parse(history.last.timestamp + 'Z'),
+          from: op.from,
+          to: op.to,
+          amount: op.amount,
+          memo: op.memo
+        }
+        
+        respond_to do |format|
+          format.html { render 'index', layout: action_name != 'card' }
+          format.atom { render layout: false }
+          format.rss { render layout: false }
+        end
       end
     end
   end
