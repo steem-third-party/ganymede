9a6a4248827bf45c45b1263f60d9a86bea0f77df
Anthony Martin <github@martin-studio.com>
2017-02-01 21:03:21 -0800

    Initial tests.

--- Gemfile
+++ Gemfile
@@ -65,11 +65,13 @@ gem 'listen'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry-rails'
 end
 
 group :development do
-  # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
+  gem 'better_errors'
+  gem 'binding_of_caller', require: false
+  gem 'rack-mini-profiler', require: false
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
@@ -77,3 +79,13 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :test do
+  gem 'capybara-angular'
+  gem 'capybara-screenshot'
+  gem 'phantomjs', require: 'phantomjs/poltergeist'
+  gem 'minitest'
+  gem 'minitest-line'
+  gem 'simplecov', require: false
+  gem 'webmock', require: false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,51 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+SimpleCov.merge_timeout 3600
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'webmock/minitest'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+require 'capybara-screenshot/minitest'
+
+if ENV["HELL_ENABLED"]
+  require "minitest/hell"
+else
+  require "minitest/pride"
+end
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'codeclimate.com:443')
+
+phantomjs_logger = if ENV['TESTOPTS'].to_s.include?('--verbose')
+  $stdout
+else
+  File.open("log/test_phantomjs.log", "a")
+end
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    phantomjs: Phantomjs.path,
+    phantomjs_logger: phantomjs_logger,
+    debug: false,
+    timeout: 15,
+    js_errors: true,
+    inspector: true,
+    extensions: [
+      'test/support/scripts/angular_errors.js'
+    ]
+  })
+end
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_driver = :poltergeist
+Capybara.default_max_wait_time = 15
+
+Capybara::Screenshot.prune_strategy = { keep: 20 }
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  # fixtures :all
 end
