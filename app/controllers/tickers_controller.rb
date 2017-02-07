d83937c6642feb8ee39d8867057dda79d564ffa9
Anthony Martin <github@martin-studio.com>
2017-02-06 19:50:38 -0800

    open graph stuff

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -22,6 +22,7 @@ class TickersController < ApplicationController
     respond_to do |format|
       format.html { }
       format.png { capture_steem_chart }
+      format.jpeg { capture_steem_chart }
       format.json { render json: @ticker }
       format.atom { render layout: false }
       format.rss { render layout: false }
@@ -37,34 +38,38 @@ private
   end
   
   def capture_steem_chart
+    fmt = params[:format]
     base_href = 'http://www.steemchart.com'
-    steem_chart = "#{base_href}/Steemchart_USD.html"
+    steem_chart = "#{base_href}/Steemchart_USD.html?md5=#{md5_title}"
   
-    filename = "#{md5_title}.png"
+    filename = "#{md5_title}.#{fmt}"
     fh = "#{Rails.root.join('tmp')}/#{filename}"
     
     File.open(fh, 'rb') do |f|
-      send_data(f.read, stream: false, filename: filename, type: "image/png", disposition: 'inline')
+      send_data(f.read, stream: false, filename: filename, type: "image/#{fmt}", disposition: 'inline')
     end and return if File.exists? fh
   
     render_options, raster_options = {}, {}
   
+    scale_factor = 4
+    raster_options[:cache_dir] = Rails.root.join('tmp')
     raster_options[:quality] = 50
-    raster_options[:crop_x] = 36
-    raster_options[:crop_y] = 36
-    raster_options[:crop_w] = 952
-    raster_options[:crop_h] = 354
+    raster_options[:crop_x] = (24 * scale_factor)
+    raster_options[:crop_y] = (36 * scale_factor)
+    raster_options[:crop_w] = (968 * scale_factor)
+    raster_options[:crop_h] = (354 * scale_factor)
+    raster_options[:zoom] = scale_factor
     raster_options[:encoding] = 'UTF-8'
   
     content = "<base href=\"#{base_href}/\" />"
     content << open(steem_chart).read
     content = content.force_encoding("UTF-8")
     kit = IMGKit.new(content, raster_options)
-    filename = "content.png" unless filename.present?
-    send_data(png = kit.to_img("png"), filename: filename, stream: false, :type => "image/png", :disposition => 'inline')
+    filename = "content.#{fmt}" unless filename.present?
+    send_data(img = kit.to_img(fmt), filename: filename, stream: false, :type => "image/#{fmt}", :disposition => 'inline')
   
     File.open(fh, 'wb') do |f|
-      f.write(png)
+      f.write(img)
     end unless File.exists? fh
   end
 end
--- app/views/tickers/show.atom.builder
+++ app/views/tickers/show.atom.builder
@@ -5,7 +5,7 @@ cache ['main-rss-ticker', expires_in: 1.hour] do
     cache ['rss-ticker', full_title] do
       feed.entry({}, url: ticker_url(@pair), id: md5_title) do |entry|
         entry.title full_title
-        entry.content "<img src=\"#{ticker_url(@pair, format: :png)}\" />"
+        entry.content "<img src=\"#{ticker_url(@pair, format: :png)}\" width=\"952\" height=\"354\" />"
       end
     end
   end
