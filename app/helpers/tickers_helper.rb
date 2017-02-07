726362386882ad6195167073a7ff8f4c5e697420
Anthony Martin <github@martin-studio.com>
2017-02-07 10:51:06 -0800

    for revs

--- app/helpers/tickers_helper.rb
+++ app/helpers/tickers_helper.rb
@@ -1,15 +1,17 @@
 module TickersHelper
   def full_title
-    return @pair if @details.nil?
-    
-    "#{@pair} - last: #{@details['last']}, " +
-    "lowest ask: #{@details['lowestAsk']}, " +
-    "highest bid: #{@details['highestBid']}, " +
-    "percent change: #{@details['percentChange']}, " +
-    "base volume: #{@details['baseVolume']}, " +
-    "quote volume: #{@details['quoteVolume']}, " +
-    "high 24hr: #{@details['high24hr']},  " +
-    "low 24hr: #{@details['low24hr']}"
+    if !!@details
+      "#{@pair} - last: #{@details['last']}, " +
+      "lowest ask: #{@details['lowestAsk']}, " +
+      "highest bid: #{@details['highestBid']}, " +
+      "percent change: #{@details['percentChange']}, " +
+      "base volume: #{@details['baseVolume']}, " +
+      "quote volume: #{@details['quoteVolume']}, " +
+      "high 24hr: #{@details['high24hr']},  " +
+      "low 24hr: #{@details['low24hr']}"
+    else
+      "#{@pair} - #{Time.now}"
+    end
   end
   
   def md5_title
