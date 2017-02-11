8a7cf00e8fcb25d8314a33314aa836bcf360c7cc
Anthony Martin <github@martin-studio.com>
2017-02-11 10:25:59 -0800

    simplified find flagwar

--- test/jobs/find_flagwar_job_test.rb
+++ test/jobs/find_flagwar_job_test.rb
@@ -4,12 +4,9 @@ class FindFlagwarJobTest < ActiveJob::TestCase
   def test_perform
     VCR.use_cassette('find_flag_war_job') do
       FindFlagwarJob.new.perform
-    end
-  end
-
-  def test_perform_with_tag
-    VCR.use_cassette('find_flag_war_job') do
-      FindFlagwarJob.new.perform(tag: 'life')
+      FindFlagwarJob.new.perform(tag: 'food')
+      
+      assert FindFlagwarJob.discussions('food').any?
     end
   end
 end
