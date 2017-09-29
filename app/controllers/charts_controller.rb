48286240bdf73f5bd9b792f5076cfa4291c7cf4f
Anthony Martin <github@martin-studio.com>
2017-09-29 16:58:29 -0700

    precision on less than 2 days

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,21 +4,35 @@ class ChartsController < ApplicationController
   
   def net_transfers
     @account_name = params[:account_name]
-    @days = (params[:days] || '14').to_i
+    @days = (params[:days] || '14.0').to_f
     
     @bids = SteemApi::Tx::Transfer.where(to: @account_name)
     @bids = @bids.where('timestamp > ?', @days.day.ago)
     @bids = @bids.where('memo LIKE ?', '%@%')
-    @bids = @bids.group_by{ |b| b.timestamp.to_date.to_s(:db) }
+    @bids = @bids.group_by do |b|
+      if @days < 2
+        b.timestamp.strftime("%H")
+      else
+        b.timestamp.to_date.to_s(:db)
+      end
+    end
 
     @refunds = SteemApi::Tx::Transfer.where(from: @account_name)
     @refunds = @refunds.where('timestamp > ?', @days.day.ago)
     @refunds = @refunds.where('memo LIKE ?', '%ID:%')
-    @refunds = @refunds.group_by{ |b| b.timestamp.to_date.to_s(:db) }
+    @refunds = @refunds.group_by do |b|
+      if @days < 2
+        b.timestamp.strftime("%H")
+      else
+        b.timestamp.to_date.to_s(:db)
+      end
+    end
     
     @net_transfers = @bids.map do |k, v|
       refunds = [@refunds[k]].flatten.compact
       refund_sum = refunds.map(&:amount).sum; [k, v.map(&:amount).sum - refund_sum]
     end
+    
+    @days = [@net_transfers.size, @days].min
   end
 end
